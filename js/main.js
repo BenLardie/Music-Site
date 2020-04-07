e886d9fb4b4a482fb234662a2b33213c2ec144f5
Ben Lardie <benlardie@gmail.com>
2020-04-07 12:50:07 -0400

    js working and so is resopnsive menu

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
-const navbarToggler = document.querySelector('.navbar-toggler');
-const navbarMenu = document.querySelector('.navbar ul');
-const navbarLinks = document.querySelectorAll('.navbar a');
+const navbarToggler = document.querySelector('.menu-button');
+const navbarMenu = document.querySelector('.nav-menu');
+const navbarLinks = document.querySelectorAll('.link');
+
+
+const navbarTogglerClick = () => {
+  console.log('clicked')
+  navbarLinks.forEach(link => {
+    link.classList.toggle('open');
+  })
+  navbarMenu.classList.toggle('open');
+};
 
 navbarToggler.addEventListener('click', navbarTogglerClick);
 
-function navbarTogglerClick() {
-  navbarToggler.classList.toggle('open-navbar-toggler');
-  navbarMenu.classList.toggle('open');
-}
-
-navbarLinks.forEach(elem => elem.addEventListener('click', navbarLinkClick));
-
-function navbarLinkClick(event) {
-  smoothScroll(event);
-  if (navbarMenu.classList.contains('open')) {
-    navbarToggler.click();
-  }
-}
 
 function smoothScroll(event) {
   event.preventDefault();
